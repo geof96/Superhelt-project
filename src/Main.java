c1475e8c52298783da78efa43446a720ba37b69a
geof96 <leyton2730@gmail.com>
2023-09-11 11:40:55 +0200

    i have added a scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         List<Superhero> superheroes = Superhero.initializeSuperheroes();
 
         // Print the superhero database
