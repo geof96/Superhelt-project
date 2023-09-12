73ffc6c4e165a8c120dd117f9c36da1d18ae90a4
geof96 <leyton2730@gmail.com>
2023-09-12 14:46:40 +0200

    1. jeg har tilføjet en scanner. 2. Jeg har lavet en do while loop, med isRunning. 3. Brugeren kan ikke registrer den nye superhelt endnu. 4. jeg har ændret fra arraylist til et array. 5. Jeg har ikke kunne for loopen til at ende med en scanner metode. 6. Jeg har heller ikke kunne printe databasen ud.
    Superhelt del opgave 2:
    1. Der er blevet lavet en Controller klasse som, skal kunne snakke med både Main og Databasen.
    2. Vi er gået fra et "Array" til en "Arrayliste" og kan nu gemme så mange superhelte som vi vil.
    3. Vi har lavet om på loopet og har valgt at bruge while istedet for.
    4. Vi har givet brugeren 3 muligheder i starten af programet.

--- src/DataStorage/Database.java
+++ src/DataStorage/Database.java
@@ -23,18 +23,3 @@ public class Database {
 
 
         }
-
-
-
-
-
-
-// Jeg har kommenteret dette væk, da brugeren selv skal indtaste informationerne.
-/*
-superheroes[0] = new Superhero("John Smith", "All Might", true, 2018, 2);
-        superheroes[1] = new Superhero("Dick Grayson", "Boy Wonder", true, 2014, 1);
-        superheroes[2] = new Superhero("Saitama", "Caped Baldy", true, 2019, 11);
-        superheroes[3] = new Superhero("Clark Kent", "Superman", false, 1998, 10);
-        superheroes[4] = new Superhero("Johnny Bravo", "", false, 2007, 4);
-        superheroes[5] = new Superhero("Jeff Bezos", "Amazon Prime", false, 2016, 9);
- */
